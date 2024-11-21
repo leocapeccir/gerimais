dfc4bf3c5d5a9ba24875176b979be83be0c77a89
Leonardo Ramos <103533833+leocapeccir@users.noreply.github.com>
2024-11-20 23:21:43 -0300

    chore: added sidebar

--- src/components/SideBarMenu/index.tsx
+++ src/components/SideBarMenu/index.tsx
@@ -4,28 +4,49 @@ import "./styles.css";
 function SideBarMenu() {
   const [open, setOpen] = useState(true);
 
+  const menuItems = [
+    { title: "Dashboard", icon: "src/assets/dashboard-svgrepo-com.svg" },
+    { title: "teste", icon: "src/assets/visitor-identification-svgrepo-com.svg" },
+    { title: "Dashboard", icon: "src/assets/dashboard-svgrepo-com.svg" },
+    { title: "teste", icon: "src/assets/visitor-identification-svgrepo-com.svg" },
+    
+  ];
+
   return (
     <div className="flex">
       <div className={`sidebar ${open ? "open" : "closed"}`}>
+        {/* Ícone de controle de abrir/fechar */}
         <img
-          src="src\assets\menu-svgrepo-com.svg"
+          src="src/assets/menu-svgrepo-com.svg"
           className={`control-icon ${!open && "open"}`}
           onClick={() => setOpen(!open)}
+          alt="Toggle Sidebar"
         />
+        
         <div className="logo-container">
           <img
-            src="src\assets\asilo.jpg"
+            src="src/assets/asilo.jpg"
             className={`logo ${open && "open"}`}
+            alt="Logo"
           />
           <h1 className={`text-white ${!open ? "hidden" : ""}`}>Nosso Lar</h1>
         </div>
         <ul>
-            
-          <li className="dashboard-title">Dashboard</li>
-          <li>teste</li>
-          <li>teste</li>
-          <li>teste</li>
-          <li>teste</li>
+          {menuItems.map((item, index) => (
+            <li key={index} className={`menu-item ${index === 0 ? 'first-item' : ""}`}>
+              {!open ? (
+                
+                <img
+                  src={item.icon}
+                  className="menu-icon"
+                  alt={item.title}
+                />
+              ) : (
+                
+                <span>{item.title}</span>
+              )}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
